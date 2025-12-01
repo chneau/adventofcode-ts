db1208d3f9d276b140dcfe2658df43cfe1f42102
chneau <charles63500@gmail.com>
2025-12-01 17:13:31 +0000

    Refactor input parsing and processing logic in p1 and p2 functions for improved clarity and efficiency

--- aoc_2025_01.ts
+++ aoc_2025_01.ts
@@ -1,9 +1,17 @@
 import z from "zod";
 import { fetchInput } from "./session";
 
-const input = await fetchInput();
+const parse = z.string().transform((x) =>
+	x.split("\n").map((line) => {
+		const action = z.enum(["L", "R"]).parse(line[0]);
+		const value = z.coerce.number().parse(line.slice(1));
+		return action === "L" ? -value : value;
+	}),
+);
 
-const example = `L68
+const input = await fetchInput().then(parse.parse);
+
+const example = parse.parse(`L68
 L30
 R48
 L5
@@ -12,41 +20,31 @@ L55
 L1
 L99
 R14
-L82`;
+L82`);
 
 export const p1ex = () => p1(example);
 
-export const p1 = (_input = input) => {
-	const lines = _input.split("\n");
+export const p1 = (nn = input) => {
 	let dial = 50;
 	let result = 0;
-	const actionSchema = z.enum(["L", "R"]);
-	const valueSchema = z.coerce.number();
-	for (const line of lines) {
-		const action = actionSchema.parse(line[0]);
-		const value = valueSchema.parse(line.slice(1));
-		dial = action === "L" ? dial - value : dial + value;
+	for (const n of nn) {
+		dial += n;
 		dial %= 100;
-		dial === 0 && ++result;
+		result += Number(dial === 0);
 	}
 	return result;
 };
 
 export const p2ex = () => p2(example);
 
-export const p2 = (_input = input) => {
-	const lines = _input.split("\n");
+export const p2 = (nn = input) => {
 	let dial = 50;
 	let result = 0;
-	const actionSchema = z.enum(["L", "R"]);
-	const valueSchema = z.coerce.number();
-	for (const line of lines) {
-		const action = actionSchema.parse(line[0]);
-		const value = valueSchema.parse(line.slice(1));
-		dial = action === "L" ? dial - value : dial + value;
+	for (const n of nn) {
+		dial += n;
 		const isOverflow = dial < 0 || dial >= 100;
 		dial = Math.abs(dial % 100);
-		isOverflow && ++result;
+		result += Number(isOverflow);
 	}
 	return result;
 };
