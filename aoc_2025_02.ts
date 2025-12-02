fe478c885823fa595091a810ff35a5ce0f49e5ea
chneau <charles63500@gmail.com>
2025-12-02 09:16:15 +0000

    Refactor p1 and p2 functions to implement correct result calculation based on input ranges

--- aoc_2025_02.ts
+++ aoc_2025_02.ts
@@ -14,12 +14,38 @@ const _example =
 export const p1ex = () => p1(_example);
 export const p1 = (input = _input) => {
 	let result = 0;
-	result += input.length;
+	for (const [start, end] of input) {
+		for (let x = start; x <= end; x++) {
+			const length = Math.floor(Math.log10(x)) + 1;
+			if (length % 2 !== 0) continue;
+			const leftPart = x % 10 ** Math.floor(length / 2);
+			const rightPart = Math.floor(x / 10 ** Math.ceil(length / 2));
+			if (leftPart !== rightPart) continue;
+			result += x;
+		}
+	}
 	return result;
 };
 export const p2ex = () => p2(_example);
-export const p2 = (input = _input) => {
+export const p2 = async (input = _input) => {
 	let result = 0;
-	result += input.length;
+	for (const [start, end] of input) {
+		for (let x = start; x <= end; x++) {
+			const length = Math.floor(Math.log10(x)) + 1;
+			for (let split = 1; split < length; split++) {
+				if (length % split !== 0) continue;
+				const parts: number[] = [];
+				for (let pos = 0; pos < length; pos += split) {
+					const part = Math.floor((x % 10 ** (pos + split)) / 10 ** pos);
+					parts.push(part);
+				}
+				const firstPart = parts[0];
+				if (firstPart == null) throw new Error("unreachable");
+				if (!parts.every((p) => p === firstPart)) continue;
+				result += x;
+				break;
+			}
+		}
+	}
 	return result;
 };
