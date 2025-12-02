c388dd4adda212006c71986fdc0a2ac47964b1d3
chneau <charles63500@gmail.com>
2025-12-02 08:31:27 +0000

    Fix formatting issue in p2 function export statement

--- index.ts
+++ index.ts
@@ -15,8 +15,7 @@ const _example = parse(\`example_input_here\`);
 export const p1ex = () => p1(_example);
 export const p1 = (input = _input) => { return input.length; };
 export const p2ex = () => p2(_example);
-export const p2 = (input = _input) => { return input.length; };
-`,
+export const p2 = (input = _input) => { return input.length; };`,
 	);
 	await Bun.$`bun run all`;
 }
