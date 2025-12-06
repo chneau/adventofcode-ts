36194622feb62a22165018e07be8bb4bb6d4e5fb
chneau <charles63500@gmail.com>
2025-12-06 13:41:12 +0000

    fix: update parsing to use async methods for consistency and correctness

--- index.ts
+++ index.ts
@@ -10,9 +10,9 @@ if (!fileExists) {
 		fileName,
 		`import z from "zod";
 import { fetchInput } from "./session";
-const parse = (input: string) => z.string().parse(input);
+const parse = (input: string) => z.string().parseAsync(input);
 const _input = await fetchInput().then(parse);
-const _example = parse(\`example_input_here\`);
+const _example = await parse(\`example_input_here\`);
 export const p1ex = () => p1(_example);
 export const p1 = (input = _input) => { let result = 0; result += input.length; return result; };
 export const p2ex = () => p2(_example);
